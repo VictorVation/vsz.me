7a692c62d02f3c93b3eb3f4b16c9f292f8c2db3d
Victor Szeto <me@victorszeto.com>
2015-01-02 13:45:06 -0500

    Squashed 'hasher/' changes from 6ca428e..d68c0b6
    d68c0b6 add analytics and favicon
    1331335 inline fonts
    7086fc6 breaks when minified
    
    git-subtree-dir: hasher
    git-subtree-split: d68c0b6c35f1663e2fca9db733958419c28c4a19

--- js/hasher.js
+++ js/hasher.js
@@ -6,23 +6,13 @@
   var $show = $('[role=show]');
   var $info = $('.info');
 
-  // $show.addEventListener('click', function(ev){
-  //     ev.stopPropagation();
-  //     ev.preventDefault();
-  //     $show.blur();
-  //     $show.textContent = $show.textContent === 'done' ? 'Learn more' : 'done';
-
-  //     $info.classList.toggle('hide');
-  //     $info.classList.toggle('show');
-  // })
-
   var update = function() {
 
     // Compute the first 16 base64 characters of iterated-SHA-256(domain + '/' + key, 2 ^ difficulty).
     var key = $key.value;
-    var domain = $domain.value;
+    var domain = $site.value;
 
-    if (!key || !domain) {
+    if (!key || !site) {
       $hash.value = '';
       return;
     }
@@ -48,12 +38,20 @@
       update();
       timeout = null;
     }), 200);
-  }
+  };
 
   // Add our event listeners.
   'propertychange change keyup input paste'.split(' ').map(function(ev) {
-    [$key, $domain].map(function(el) {
+    [$key, $site].map(function(el) {
       return el.addEventListener(ev, debouncedUpdate);
     });
   });
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46624647-1', 'auto');
+  ga('send', 'pageview');
 })();
